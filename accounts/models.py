77cb9be4efeaaade70b257296a53781b3d522ce7
F_Muhammad <maga.mutsolgov@bk.ru>
2025-07-03 20:38:20 +0300

    Accounts serializers,views,urls

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-from users.models import User
+from django.conf import settings
 
 class Account(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    number = models.CharField(max_length=20, unique=True)
-    description = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accounts')
+    number = models.CharField("Номер лицевого счета", max_length=20, unique=True)
+    description = models.CharField("Описание/Название", max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.number} ({self.user.username})"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Account
+from .serializers import AccountSerializer
 
-# Create your views here.
+class AccountViewSet(viewsets.ModelViewSet):
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
